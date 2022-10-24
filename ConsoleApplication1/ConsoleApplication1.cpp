f35310dbb8de14686ead0b42176607b685e68859
Duong Xuan Bach <duongbach202156@gmail.com>
2022-10-24 18:37:12 +0700

    Delete Tung dep trai

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -6,9 +6,7 @@
 int main()
 {
     std::cout << "Hello Tung dep trai\n";
-    std::cout << "Hello Tung dep trai\n";
-    std::cout << "Hello Tung dep trai\n";
-    std::cout << "Hello Tung dep trai\n";
+    
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
